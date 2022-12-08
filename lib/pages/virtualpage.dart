5ac4053b69bb54c8b3e316f657d659f171b4a972
chirtu <chitufatuma45@gmail.com>
2022-12-08 16:22:59 +0300

    virtual box page updated

--- lib/pages/virtualpage.dart
+++ lib/pages/virtualpage.dart
@@ -58,10 +58,82 @@ class _virtualpageState extends State<virtualpage> {
                 ),
               ),
             ),
-
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Card(
+                child: Container(
+                  // margin: EdgeInsets.all(10),
+                  height: 70,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image(image: AssetImage("assets/v.png")),
+                      Text("Connect to Virtual Box",style: TextStyle(fontSize: 18),),
+                      InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>  _virtual()));
+                        },
+                          child: Icon(CupertinoIcons.forward,color: kprimarycolor,size: 20,))
+                    ],
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ) ,
+    );
+  }
+  _virtual(){
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Card(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                height: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 20,),
+                    Text("Enter Virtual box details to connect",style: TextStyle(fontSize: 14,color: kprimarycolor),),
+                    SizedBox(height: 10,),
+                    Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 12.0,bottom: 3,left: 20,right: 20),
+                        child: Text("Mobile Number",style: TextStyle(color: kprimarycolor,fontSize: 14),),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        border: Border.all(color: kgrey),
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 12.0,bottom: 3,left: 20,right: 20),
+                        child: Text("Pass code",style: TextStyle(color: kprimarycolor,fontSize: 14),),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        border: Border.all(color: kgrey),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
